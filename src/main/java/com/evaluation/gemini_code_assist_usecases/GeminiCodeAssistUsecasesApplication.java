17540337eb2ccdde4b9410e91f8800f851f68083
Ishwar Chandra <2ishwarchandra@gmail.com>
2024-06-18 20:11:08 +0530

    Initial commit

---
+++ src/main/java/com/evaluation/gemini_code_assist_usecases/GeminiCodeAssistUsecasesApplication.java
@@ -0,0 +1,13 @@
+package com.evaluation.gemini_code_assist_usecases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiCodeAssistUsecasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiCodeAssistUsecasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evaluation/gemini_code_assist_usecases/GeminiCodeAssistUsecasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluation.gemini_code_assist_usecases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiCodeAssistUsecasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
